df7a8df34e790a593c76f8b0adcb639ab5c4a11e
MRosales0390 <marco_rosales@hotmail.es>
2022-07-15 03:07:11 -0500

    Added cors to server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
 const postRouter = require("./routes/post.route")
@@ -6,6 +7,7 @@ const userRouter = require("./routes/user.route")
 const authRouter = require("./routes/auth.route")
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/login", authRouter)
 app.use("/user", userRouter)
